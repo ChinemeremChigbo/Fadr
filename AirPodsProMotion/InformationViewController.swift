ae5b22ede35381631f4878cd0cf501e5a3905472
Chinemerem <chinemeremchigbo@Outlook.com>
2024-02-19 23:24:36 -0500

    Working headphone and phone data
    This is the initial commit. It introduces an XCode project with 4 main tabs, "Information View", "Rotate the Cube View", "Table scrolling by Head Motion" and Export to CSV file

---
+++ AirPodsProMotion/InformationViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  AirPodsProMotion
+//
+//  Created by Yoshio on 2020/09/22.
+//
+
+import UIKit
+import CoreMotion
+
+class InformationViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
+    
+    lazy var textViewHeadphone: UITextView = {
+        let view = UITextView()
+        view.frame = CGRect(x: self.view.bounds.minX + (self.view.bounds.width / 10),
+                            y: self.view.bounds.minY + (self.view.bounds.height / 10),
+                            width: self.view.bounds.width, height: self.view.bounds.height / 2)
+        view.text = "Looking for headphones"
+        view.font = view.font?.withSize(14)
+        view.isEditable = false
+        return view
+    }()
+    
+    lazy var textViewPhone: UITextView = {
+        let view = UITextView()
+        view.frame = CGRect(x: self.view.bounds.minX + (self.view.bounds.width / 10),
+                            y: self.view.bounds.height / 2  + (self.view.bounds.height / 10),
+                            width: self.view.bounds.width, height: self.view.bounds.height / 2)
+        view.text = "Looking for phone"
+        view.font = view.font?.withSize(14)
+        view.isEditable = false
+        return view
+    }()
+    
+    
+    let headphone = CMHeadphoneMotionManager()
+    let phone = CMMotionManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Information View"
+        view.backgroundColor = .systemBackground
+        view.addSubview(textViewHeadphone)
+        view.addSubview(textViewPhone)
+        
+        
+        
+        //        headphone.delegate = self
+        
+        guard headphone.isDeviceMotionAvailable else {
+            AlertView.alert(self, "Sorry", "Your headphones are not supported.")
+            textViewHeadphone.text = "Sorry, Your headphones are not supported."
+            return
+        }
+        
+        guard phone.isDeviceMotionAvailable else {
+            AlertView.alert(self, "Sorry", "Your phone is not supported.")
+            textViewPhone.text = "Sorry, Your phone is not supported."
+            return
+        }
+        
+        
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 2
+        
+        headphone.startDeviceMotionUpdates(to: queue) { [weak self] motion, error in
+            guard let motion = motion, error == nil else { return }
+            self?.printData(motion, for: "Headphone", textview: self?.textViewHeadphone ?? UITextView())
+        }
+        
+        phone.startDeviceMotionUpdates(to: queue) { [weak self] motion, error in
+            guard let motion = motion, error == nil else { return }
+            self?.printData(motion, for: "Phone", textview: self?.textViewPhone ?? UITextView())
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        headphone.stopDeviceMotionUpdates()
+    }
+    
+    
+    func printData(_ data: CMDeviceMotion, for source: String, textview: UITextView) {
+        DispatchQueue.main.async {
+            print("\(source) data: \(data)")
+            textview.text = """
+                \(source) Data:
+                Quaternion:
+                    x: \(data.attitude.quaternion.x)
+                    y: \(data.attitude.quaternion.y)
+                    z: \(data.attitude.quaternion.z)
+                    w: \(data.attitude.quaternion.w)
+                Attitude:
+                    pitch: \(data.attitude.pitch)
+                    roll: \(data.attitude.roll)
+                    yaw: \(data.attitude.yaw)
+                """
+        }
+        
+    }
+}
---
+++ AirPodsProMotion/SK3DViewController.swift
@@ -0,0 +1,119 @@
+//
+//  SK3DViewController.swift
+//  AirPodsProMotion
+//
+//  Created by Yoshio on 2020/09/23.
+//
+
+import UIKit
+import SceneKit
+import CoreMotion
+
+class SK3DViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
+    
+    //AirPods Pro => APP :)
+    let APP = CMHeadphoneMotionManager()
+    // cube
+    var cubeNode: SCNNode!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBackground
+        self.title = "Simple 3D View"
+        
+        APP.delegate = self
+
+        SceneSetUp()
+        
+        guard APP.isDeviceMotionAvailable else {
+            AlertView.alert(self, "Sorry", "Your device is not supported.")
+            return
+        }
+        APP.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {[weak self] motion, error  in
+            guard let motion = motion, error == nil else { return }
+            self?.NodeRotate(motion)
+        })
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        APP.stopDeviceMotionUpdates()
+    }
+    
+    
+    func NodeRotate(_ motion: CMDeviceMotion) {
+        let data = motion.attitude
+
+        cubeNode.eulerAngles = SCNVector3(-data.pitch, -data.yaw, -data.roll)
+    }
+}
+
+
+// SceneKit
+extension SK3DViewController {
+    
+    func SceneSetUp() {
+        let scnView = SCNView(frame: self.view.frame)
+        scnView.backgroundColor = UIColor.black
+        scnView.allowsCameraControl = false
+        scnView.showsStatistics = true
+        view.addSubview(scnView)
+
+        // Set SCNScene to SCNView
+        let scene = SCNScene()
+        scnView.scene = scene
+
+        // Adding a camera to a scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        scene.rootNode.addChildNode(cameraNode)
+
+        // Adding an omnidirectional light source to the scene
+        let omniLight = SCNLight()
+        omniLight.type = .omni
+        let omniLightNode = SCNNode()
+        omniLightNode.light = omniLight
+        omniLightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(omniLightNode)
+
+        // Adding a light source to your scene that illuminates from all directions.
+        let ambientLight = SCNLight()
+        ambientLight.type = .ambient
+        ambientLight.color = UIColor.darkGray
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = ambientLight
+        scene.rootNode.addChildNode(ambientLightNode)
+
+    
+        // Adding a cube(face) to a scene
+        let cube:SCNGeometry = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0.5)
+        let eye:SCNGeometry = SCNSphere(radius: 0.3)
+        let leftEye = SCNNode(geometry: eye)
+        let rightEye = SCNNode(geometry: eye)
+        leftEye.position = SCNVector3(x: 0.6, y: 0.6, z: 1.5)
+        rightEye.position = SCNVector3(x: -0.6, y: 0.6, z: 1.5)
+        
+        let nose:SCNGeometry = SCNSphere(radius: 0.3)
+        let noseNode = SCNNode(geometry: nose)
+        noseNode.position = SCNVector3(x: 0, y: 0, z: 1.5)
+        
+        let mouth:SCNGeometry = SCNBox(width: 1.5, height: 0.2, length: 0.2, chamferRadius: 0.4)
+        let mouthNode = SCNNode(geometry: mouth)
+        mouthNode.position = SCNVector3(x: 0, y: -0.6, z: 1.5)
+        
+        
+        cubeNode = SCNNode(geometry: cube)
+        cubeNode.addChildNode(leftEye)
+        cubeNode.addChildNode(rightEye)
+        cubeNode.addChildNode(noseNode)
+        cubeNode.addChildNode(mouthNode)
+        cubeNode.position = SCNVector3(x: 0, y: 0, z: 0)
+        scene.rootNode.addChildNode(cubeNode)
+    }
+}
