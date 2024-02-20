a96a4ec565d53430194cfa73112ea850862122be
Chinemerem <chinemeremchigbo@Outlook.com>
2024-02-20 17:04:19 -0500

    Remove properties from 3D face
    Remove nodeNode, mouthNode, leftEye, and rightEye from cubeNode

--- AirPodsProMotion/SK3DViewController.swift
+++ AirPodsProMotion/SK3DViewController.swift
@@ -84,26 +84,8 @@ extension SK3DViewController {
     
         // Adding a cube(face) to a scene
         let cube:SCNGeometry = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0.5)
-        let eye:SCNGeometry = SCNSphere(radius: 0.3)
-        let leftEye = SCNNode(geometry: eye)
-        let rightEye = SCNNode(geometry: eye)
-        leftEye.position = SCNVector3(x: 0.6, y: 0.6, z: 1.5)
-        rightEye.position = SCNVector3(x: -0.6, y: 0.6, z: 1.5)
-        
-        let nose:SCNGeometry = SCNSphere(radius: 0.3)
-        let noseNode = SCNNode(geometry: nose)
-        noseNode.position = SCNVector3(x: 0, y: 0, z: 1.5)
-        
-        let mouth:SCNGeometry = SCNBox(width: 1.5, height: 0.2, length: 0.2, chamferRadius: 0.4)
-        let mouthNode = SCNNode(geometry: mouth)
-        mouthNode.position = SCNVector3(x: 0, y: -0.6, z: 1.5)
-        
         
         cubeNode = SCNNode(geometry: cube)
-        cubeNode.addChildNode(leftEye)
-        cubeNode.addChildNode(rightEye)
-        cubeNode.addChildNode(noseNode)
-        cubeNode.addChildNode(mouthNode)
         cubeNode.position = SCNVector3(x: 0, y: 0, z: 0)
         scene.rootNode.addChildNode(cubeNode)
     }
