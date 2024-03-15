75f4d1bcdfdb191769b53ed46859d6258e9823a5
Chinemerem <chinemeremchigbo@Outlook.com>
2024-03-15 00:55:25 -0400

    Add Extensions

---
+++ FadeBot/Extensions/ColorExtensions.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+
+extension UIColor {
+    
+    
+    // MARK: - Custom Colors
+    
+    static let imageBorderGray = UIColor(
+        red: 209/255, green: 209/255, blue: 209/255, alpha: 1)
+    
+    
+    // MARK: - Support Dark Mode colors
+    
+    // Support custom colors for dark and light themes
+    static func dynamicColor(light: UIColor, dark: UIColor) -> UIColor {
+        return UIColor { $0.userInterfaceStyle == .dark ? dark : light }
+    }
+}
---
+++ FadeBot/Extensions/StringExtensions.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+
+extension String {
+  
+
+  // MARK: - Add Style to any String
+  
+  func toStyleString(with fontSize: CGFloat, and fontWeight: UIFont.Weight) -> NSAttributedString {
+    
+    let attributedString = NSMutableAttributedString(string: self)
+    
+    let attributedStringKey = [
+      NSAttributedString.Key.font: UIFont
+        .systemFont(ofSize: fontSize, weight: fontWeight)
+    ]
+    
+    attributedString.setAttributes(
+      attributedStringKey,
+      range: NSRange(location: 0, length: self.count))
+    
+    return attributedString
+  }
+  
+  
+  // MARK: - Capitalize first letter
+  
+  func capitalizingFirstLetter() -> String {
+    return prefix(1).capitalized + dropFirst()
+  }
+  
+  mutating func capitalizeFirstLetter() {
+    self = self.capitalizingFirstLetter()
+  }
+  
+}
