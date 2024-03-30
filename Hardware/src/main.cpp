3a91cafd956a1c91524045fa29ad6f72ab602700
Krish Shah <krish.life21@gmail.com>
2024-03-30 07:09:52 -0400

    allow for servo!

--- Hardware/src/main.cpp
+++ Hardware/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 #include "bletest.h"
 
 // NOTES:
@@ -21,6 +22,9 @@ int pwmOutput = 65535;
 
 int MAX_POSITION = 4095;
 int MIN_POSITION = 0;
+
+int servoPin = 4;
+Servo myservo; // create servo object to control a servo
 
 class MyServerCallbacks : public BLEServerCallbacks
 {
@@ -45,6 +49,7 @@ class MessageCallbacks : public BLECharacteristicCallbacks
 
         int servoValue = atoi(data.c_str());
 
+        // not accurate if it is servo instead of actuator
         servoValue = constrain(servoValue, MIN_POSITION, MAX_POSITION);
 
         pos = servoValue;
@@ -115,11 +120,25 @@ void moveToPosition(int targetPosition)
 void setup()
 {
     Serial.begin(115200);
+
+    // Actuator setup
     pinMode(enablePin, OUTPUT);
     pinMode(in1Pin, OUTPUT);
     pinMode(in2Pin, OUTPUT);
 
     pinMode(potPin, INPUT);
+
+    // Servo setup
+    // Allow allocation of all timers
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
+    myservo.setPeriodHertz(50);          // standard 50 hz servo
+    myservo.attach(servoPin, 500, 2400); // attaches the servo on pin 18 to the servo object
+                                         // using default min/max of 1000us and 2000us
+                                         // different servos may require different min/max settings
+                                         // for an accurate 0 to 180 sweep
 
     // Setup BLE Server
     BLEDevice::init(DEVICE_NAME);
@@ -154,11 +173,11 @@ void setup()
     analogWrite(enablePin, pwmOutput);
 
     Serial.println("Ready");
-
 }
 
 void loop()
 {
     int potValue = analogRead(potPin);
     moveToPosition(pos);
+    myservo.write(pos);
 }
