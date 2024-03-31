f161ccf894b6d028163e00dbb20872bf119db9ba
Chinemerem <chinemeremchigbo@Outlook.com>
2024-03-30 21:52:45 -0400

    Comment out timer allocation

--- Hardware/src/main.cpp
+++ Hardware/src/main.cpp
@@ -130,11 +130,11 @@ void setup()
 
     // Servo setup
     // Allow allocation of all timers
-    ESP32PWM::allocateTimer(0);
-    ESP32PWM::allocateTimer(1);
-    ESP32PWM::allocateTimer(2);
-    ESP32PWM::allocateTimer(3);
-    myservo.setPeriodHertz(50);          // standard 50 hz servo
+    // ESP32PWM::allocateTimer(0);
+    // ESP32PWM::allocateTimer(1);
+    // ESP32PWM::allocateTimer(2);
+    // ESP32PWM::allocateTimer(3);
+    // myservo.setPeriodHertz(50);          // standard 50 hz servo
     myservo.attach(servoPin, 500, 2400); // attaches the servo on pin 18 to the servo object
                                          // using default min/max of 1000us and 2000us
                                          // different servos may require different min/max settings
